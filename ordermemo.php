68b2e07d0b008de816b749f17ad106749b069700
dai_m <d.matsuda@etbs.jp>
2026-08-22 18:44:26 +0900

    1.0.3 へ（アンインストール時に定型文を完全削除していた不具合の修正を配信）

--- ordermemo.php
+++ ordermemo.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       OrderMemo
  * Description:       WooCommerceの注文編集画面にテンプレート挿入機能を追加し、定型文（差し込みタグ対応）をワンクリックで注文メモに入力できるプラグイン
- * Version:           1.0.2
+ * Version:           1.0.3
  * Requires PHP:      7.4
  * Requires Plugins:  woocommerce
  * Author:            DAI
